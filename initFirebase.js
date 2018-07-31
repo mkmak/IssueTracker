fb149ca49d1b8c4dc68725d77a10e83fb83a2150
Xunle <xumai@ucsd.edu>
2018-07-30 20:22:42 -0700

    firebase auth

---
+++ initFirebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBpT4Da1QDwhhFQLPmTFZqofGSExaDlKhc",
+    authDomain: "b-hw2-38225.firebaseapp.com",
+    databaseURL: "https://b-hw2-38225.firebaseio.com",
+    projectId: "b-hw2-38225",
+    storageBucket: "b-hw2-38225.appspot.com",
+    messagingSenderId: "414413271526"
+  };
+  firebase.initializeApp(config);
